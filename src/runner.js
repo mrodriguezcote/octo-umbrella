dff58d860d6991e446b0f947f625d626bc183154
Marco Rodriguez <marco.rodriguez@blueacorn.com>
2016-05-29 01:36:25 -0400

    minor adjustments

--- src/runner.js
+++ src/runner.js
@@ -6,7 +6,11 @@ var level3 = require('./setup/config/level3');
 
 var command = process.argv[2];
 
-if(command.includes('level')) {
+if(!command) {
+	console.log('usage: npm start [testLevel/Set]');
+}
+
+else if(command.includes('level')) {
 	upToLevel = parseInt(command[5]);
 	if (upToLevel == 1) {
 		level1.runLevel();
@@ -17,14 +21,19 @@ if(command.includes('level')) {
 	else if (upToLevel == 3) {
 		level3.runLevel();
 	}
+	else {
+		console.log('Unidentified Level');
+	}
 }
 else {
 	var testLevels = roster.read();
 	var setLevel = 0;
+	var found = false;
 	_.each(testLevels, function(sets) {
 		setLevel++;
 		_.each(sets, function(set) {
 			if(command == set) {
+				found = true;
 				var upToLevel = setLevel-1;
 				if (upToLevel == 0) {
 					level1.runLevel(command);
@@ -38,6 +47,7 @@ else {
 			}
 		})
 	})
+	if (found == false) { console.log('Unidentified Set'); }
 }
 
 
